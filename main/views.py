b924567b2ffea8b857d5fffdd46f53098e8d557f
nbadnjarevic <nbadnjarevic16@raf.rs>
2019-03-23 21:47:14 +0100

    Pocetna strana

--- main/views.py
+++ main/views.py
@@ -3,13 +3,32 @@ from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
-
-def mapa(request):
     import requests
-    url = 'https://api.airvisual.com/v2/stations?city=Beijing&state=Beijing&country=China&key=wPHw9rvRRjYp9veR4'
+    import json
+    url = 'https://api.airvisual.com/v2/nearest_city?key=wPHw9rvRRjYp9veR4'
     payload = {}
     headers = {}
     response = requests.request('GET', url, headers = headers, data = payload, allow_redirects=False, timeout=500)
-    print(response.text)
-    return render(request, 'mapa.html', {})
+    j = json.loads(response.text)
+    location = j["data"]["city"]
+    temperature = j["data"]["current"]["weather"]["tp"]
+    pressure = j["data"]["current"]["weather"]["pr"]
+    humidity = j["data"]["current"]["weather"]["hu"]
+    wind_speed = j["data"]["current"]["weather"]["ws"]
+    wind_direction = j["data"]["current"]["weather"]["wd"]
+    icon = j["data"]["current"]["weather"]["ic"]
+    print(icon)
+    aqius = j["data"]["current"]["pollution"]["aqius"]
+    context = {"location":location,
+        "temperature":temperature,
+        "pressure":pressure,
+        "humidity":humidity,
+        "wind_speed":wind_speed,
+        "wind_direction":wind_direction, 
+        "aqius":aqius,
+        "icon":icon}
+
+    return render(request, 'index.html', context)
+
+def mapa(request):
+    return render(request, 'mapa.html', context)
